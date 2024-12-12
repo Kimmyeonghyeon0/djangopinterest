ebc4f97abc2f45f0bbc8ca44cb05a21ff05c9c23
Kimmyeonghyeon0 <Kimmyeonghyeom0>
2024-12-12 13:42:20 +0900

    SECRET_KEY설정

--- pinterestmain/settings.py
+++ pinterestmain/settings.py
@@ -12,15 +12,29 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import environ, os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(
+    env_file= os.path.join(BASE_DIR, '.env')
+)
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1#(7t)ngut_6c@pcy_2)1+z3th^56a0ye#1+q*i36tf^%!@$m&"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
